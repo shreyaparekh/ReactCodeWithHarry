f4176c91fb56ebb5abfb13dfc7f11250b85d1d7f
shreyaparekh <shreyaparekh15@gmail.com>
2023-09-14 21:06:39 +0530

    commit14_09

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import About from './components/About';
 import Alert from './components/Alert';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+
 
 function App() {
   const [mode,setMode]=useState("light");//where dark mode is enable or not
@@ -24,30 +27,50 @@ if(mode=='light'){
   setMode("dark");
   document.body.style.backgroundColor='#042743';
   showAlert("Dark Mode Enable","success");
+document.title='Dark Mode';
 
-
+setInterval(() => {
+document.title='text utils amazinng mode';
+}, 2000);
+setInterval(() => {
+  document.title='install text utils amazinng mode';
+  }, 2000);
 }else{
   setMode("light");
   document.body.style.backgroundColor='white';
   showAlert("light Mode Enable","success")
+  document.title='Light Mode';
+
 
 
 }
 
   }
   return (
-    <>
-      <Navbar title="textUtiles" aboutText="About" mode={mode} toggleMode={toggleMode}/>
-      <Alert alert={alert}/>
-      <div className="container my-3">
-      <TextForm heading="Enter Text for paragarpgh" mode={mode} showAlert={showAlert} />
-      <About />
-      </div>
-      {/* <Navbar  /> */}
-
-      {/* Rest of your code */}
-    </>
+    <Router>
+    <Navbar title="textUtiles" aboutText="About" mode={mode} toggleMode={toggleMode}/>
+    <Alert alert={alert}/>
+    <div className="container my-3">
+      <Routes>
+        <Route path="/home" element={<TextForm heading="Enter Text for paragraph" mode={mode} showAlert={showAlert} />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  </Router>
   );
+  // return (
+  //   <>
+  //     <Navbar title="textUtiles" aboutText="About" mode={mode} toggleMode={toggleMode}/>
+  //     <Alert alert={alert}/>
+  //     <div className="container my-3">
+  //     <TextForm heading="Enter Text for paragarpgh" mode={mode} showAlert={showAlert} />
+  //     <About />
+  //     </div>
+  //     {/* <Navbar  /> */}
+
+  //     {/* Rest of your code */}
+  //   </>
+  // );
 }
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+
 export class Navbar extends Component {
   render() {
     return (
@@ -12,10 +13,14 @@ export class Navbar extends Component {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">Home</a>
+            {/* <a className="nav-link active" aria-current="page" href="/">Home</a> */}
+
+              <a className="nav-link active" aria-current="page" href="/home">Home</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/">{this.props.aboutText}</a>
+            {/* <a  className="nav-link" href="/">{this.props.aboutText}</a> */}
+
+              <a  className="nav-link" href="/about">{this.props.aboutText}</a>
             </li>
          
           </ul>
